5f2f2d124a1acaef7b205271848587ec062564e5
AndrewHeyligerIT <112223511+AndrewHeyligerIT@users.noreply.github.com>
2024-08-14 21:48:08 -0400

    updated git branching.sh

--- branching.sh
+++ branching.sh
@@ -2,7 +2,14 @@
 
 # The Basic Rebase
 
+
+#Do not rebase commits that exist outside your repository and that people may have based work on. #
+
 # Reapply commits on top of another base tip. 
 git rebase <branch-thats-further-ahead>
 
+#You can rebase the server branch onto the master branch without having to check it out first by running
+git rebase <basebranch> <topicbranch>
 
+# You can delete your branch after you merged it
+git branch -d <branch-thats-been-merged>
